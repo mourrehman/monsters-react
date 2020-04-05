754382f12b7e52317be60d23aae014b563c0fad3
Mohammed Riyaz <contact.riyaz9@gmail.com>
2020-04-05 18:44:31 +0530

    Updates

---
+++ precache-manifest.d769b223773a0da1d2ccdcea09d68c38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa101e51250adcf916e0e314d0c513c9",
+    "url": "/monsters-react/index.html"
+  },
+  {
+    "revision": "48b5aec89d8d4279a9c1",
+    "url": "/monsters-react/static/css/main.e0de4f4a.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-react/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-react/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48b5aec89d8d4279a9c1",
+    "url": "/monsters-react/static/js/main.d511bd54.chunk.js"
+  },
+  {
+    "revision": "67c2add208faf4f9590b",
+    "url": "/monsters-react/static/js/runtime-main.55c36f79.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
